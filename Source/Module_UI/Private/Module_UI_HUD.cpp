83bd20079004d260728f6d24cf0940e50b650407
Andrey <getboros@gmail.com>
2025-05-03 09:36:42 +0300

    backup, minimap can be destroyd

--- Source/Module_UI/Private/Module_UI_HUD.cpp
+++ Source/Module_UI/Private/Module_UI_HUD.cpp
@@ -23,3 +23,259 @@ FReply UModule_UI_HUD::NativeOnMouseMove(const FGeometry& InGeometry, const FPoi
     return FReply::Unhandled(); // ¬озвращаем Unhandled, чтобы событие передавалось дальше
 }
 //------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------
+
+
+/*
+
+#include "Personal_Hub_Game_Mode.h"
+#include "Personal_Hub_Character.h"
+
+#include "Components/SizeBox.h"
+#include "Components/Image.h"
+#include "Components/ProgressBar.h"
+#include "Components/Border.h"
+
+#include "GameFramework/GameUserSettings.h"
+
+// UADrag_Widget
+void UADrag_Widget::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+//-------------------------------------------------------------------------------------------------------------
+void UADrag_Widget::Update_State(const UImage* image, const FVector2D& desired_size) const
+{
+	SizeBox_Root->SetWidthOverride(desired_size.X);
+	SizeBox_Root->SetHeightOverride(desired_size.Y);  // Set Widget Box Size
+	Border_Root->SetBrush(image->GetBrush());
+}
+//-------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+// UAPersonal_Main_Button
+void UAPersonal_Main_Button::NativeConstruct()
+{
+	Init();
+
+	Super::NativeConstruct();
+}
+//-------------------------------------------------------------------------------------------------------------
+FReply UAPersonal_Main_Button::NativeOnMouseButtonDown(const FGeometry& in_geometry, const FPointerEvent& in_mouse_event)
+{
+	Super::NativeOnMouseButtonDown(in_geometry, in_mouse_event);
+
+	Drag_Offset = in_geometry.AbsoluteToLocal(in_mouse_event.GetScreenSpacePosition());  // Save Drag offset
+
+	return UWidgetBlueprintLibrary::DetectDragIfPressed(in_mouse_event, this, EKeys::LeftMouseButton).NativeReply;  // if start drag
+}
+//-------------------------------------------------------------------------------------------------------------
+void UAPersonal_Main_Button::NativeOnDragDetected(const FGeometry& in_geometry, const FPointerEvent& in_mouse_event, UDragDropOperation*& out_operation)
+{// what happens when the player is, in fact, dragging the Image_Root around on the screen.
+
+	UADrag_Widget* drag_visual = 0;
+
+	Super::NativeOnDragDetected(in_geometry, in_mouse_event, out_operation);
+
+	// 1.0. Display Widget
+	drag_visual = CreateWidget<UADrag_Widget>(GetWorld(), Drag_Widget_Class);  // Widget to show where we drag this widget
+	drag_visual->Update_State(Image_Root, GetDesiredSize());
+
+	// 1.1. Operation Drag n Drop Init  NewDragDrop->Offset = InGeometry.AbsoluteToLocal(InMouseEvent.GetScreenSpacePosition());
+	Drag_Drop_Operation = NewObject<UADrag_Drop_Operation>(this, Drag_Drop_O_Class);
+	Drag_Drop_Operation->DefaultDragVisual = drag_visual;
+	Drag_Drop_Operation->Payload = this;  // Is it correct?
+	Drag_Drop_Operation->Pivot = EDragPivot::MouseDown;  // where the Drag Widget Visual appears while being dragged relative to the pointer performing the drag operation.
+	Drag_Drop_Operation->Widget_Reference = this;  // For recreating
+	Drag_Drop_Operation->Drag_Offset = Drag_Offset;  // Off set from mouse to correct add to viewport
+
+	out_operation = Drag_Drop_Operation;  // send operation to drop
+}
+//-----------------------------------------------------------------------------------------------------------
+bool UAPersonal_Main_Button::NativeOnDrop(const FGeometry& in_geometry, const FDragDropEvent& in_drag_drop_event, UDragDropOperation* in_operation)
+{
+	UADrag_Drop_Operation* drop_operations;
+	UAPersonal_Main_Button* drop_widget_ref;
+
+	Super::NativeOnDrop(in_geometry, in_drag_drop_event, in_operation);
+	drop_operations = Cast<UADrag_Drop_Operation>(in_operation);
+	drop_widget_ref = Cast<UAPersonal_Main_Button>(drop_operations->Widget_Reference);
+
+	// 1.0. Update On Droped Button State
+	Is_Empty = false;
+	Update_State();  // Update button state
+
+	// 1.1. Update Draged Button State
+	drop_widget_ref->Is_Empty = true;
+	drop_widget_ref->Update_State();  // Update button state
+
+	return true;
+}
+//-----------------------------------------------------------------------------------------------------------
+void UAPersonal_Main_Button::Init() const
+{
+	check(Drag_Drop_O_Class);
+	check(Drag_Widget_Class);
+}
+//-----------------------------------------------------------------------------------------------------------
+
+
+
+
+
+// UAPersonal_Main_Menu
+void UAPersonal_Main_Menu::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+//-------------------------------------------------------------------------------------------------------------
+
+
+
+
+// UAPersonal_Main_HUD
+void UAPersonal_Main_HUD::NativeConstruct()
+{
+	WMorph_Param_Array = 0;
+	EHUD_Status = EHUD_State::None;
+
+	Super::NativeConstruct();
+}
+//-------------------------------------------------------------------------------------------------------------
+bool UAPersonal_Main_HUD::NativeOnDrop(const FGeometry& in_geometry, const FDragDropEvent& in_drag_drop_event, UDragDropOperation* in_operation)
+{
+	UUserWidget* slot_widget = 0;  // need to save after
+	FVector2D view_pos = {};
+
+	Super::NativeOnDrop(in_geometry, in_drag_drop_event, in_operation);
+
+	Drag_N_Drop_Operation = Cast<UADrag_Drop_Operation>(in_operation);  // or UADrag_Drop_Operation
+	slot_widget = Drag_N_Drop_Operation->Widget_Reference;
+	slot_widget->RemoveFromParent();
+	slot_widget->AddToViewport();
+	view_pos = in_geometry.AbsoluteToLocal(in_drag_drop_event.GetScreenSpacePosition()) - Drag_N_Drop_Operation->Drag_Offset;  // Set where to place location
+	slot_widget->SetPositionInViewport(view_pos, false);
+
+	return true;
+}
+//-------------------------------------------------------------------------------------------------------------
+void UAPersonal_Main_HUD::Handler(const EWidget_Action widget_main_hud, void* ptr)
+{
+	switch (widget_main_hud)
+	{
+	case EWidget_Action::None:
+		break;
+
+
+	case EWidget_Action::Init_Morph_Param:
+		WMorph_Param_Array = static_cast<double*>(ptr);  // If action Morph convert to double array, Save to Morphs_Options and send to widget
+		Update_Morph_Sliders();
+		break;
+
+	case EWidget_Action::Init_Morph_Func:
+		Update_Morph = static_cast<std::function<void()> *>(ptr);
+		break;
+
+	case EWidget_Action::Update_Morph_Slider:
+		Update_Morph_Sliders();
+		break;
+
+	case EWidget_Action::Menu_Main_Toogle:
+		Handler_State_HUD(EHUD_State::Toogle_Menu_Main);
+		break;
+
+	case EWidget_Action::Inventory_Toogle:
+		Handler_State_HUD(EHUD_State::Toogle_Inventory);
+		break;
+	default:
+		break;
+	}
+}
+//-------------------------------------------------------------------------------------------------------------
+void UAPersonal_Main_HUD::Update_Morph_Sliders()
+{
+	for (size_t i = 0; i < 5; i++)
+		Morphs_Param.Add(WMorph_Param_Array[i]);
+
+	Handler_State_HUD(EHUD_State::Update_Morph);  // In BP Call update morps
+}
+//-------------------------------------------------------------------------------------------------------------
+void UAPersonal_Main_HUD::Handle_Game_State()
+{
+	(*Update_Morph)();
+}
+//-------------------------------------------------------------------------------------------------------------
+void UAPersonal_Main_HUD::Set_Morph_State(const int index, double value)
+{
+	WMorph_Param_Array[index] = value;
+	(*Update_Morph)();
+}
+//------------------------------------------------------------------------------------------------------------
+
+
+
+
+// AAPersonal_HUD
+AAPersonal_HUD::AAPersonal_HUD()
+	: Widget_HUD_State(), Widget_HUD(0), Widget_HUD_Class(0)
+{
+}
+//-----------------------------------------------------------------------------------------------------------
+void AAPersonal_HUD::BeginPlay()
+{
+	//if (!Widget_HUD_Class != 0)
+	//	return;
+	//Widget_HUD = CreateWidget<UAPersonal_Main_HUD>(GetWorld(), Widget_HUD_Class);
+	//Widget_HUD->AddToViewport();
+
+	Super::BeginPlay();
+}
+//-----------------------------------------------------------------------------------------------------------
+void AAPersonal_HUD::HUD_Handler(const EWidget_Action& widgete_action, void* ptr)
+{
+	switch (widgete_action)
+	{
+	case EWidget_Action::None:
+		Widget_HUD_State = static_cast<EWidget_Action*>(ptr);  // Initialize
+		break;
+	case EWidget_Action::Update_HUD_State:
+		*Widget_HUD_State = (EWidget_Action)Widget_HUD->EHUD_Status;
+		break;
+	default:
+		Widget_HUD->Handler(widgete_action, ptr);
+		break;
+	}
+}
+//-----------------------------------------------------------------------------------------------------------
+
+
+
+
+// APersonalHubGameMode
+AAPersonal_Hub_Game_Mode::AAPersonal_Hub_Game_Mode()
+	: Personal_HUD(0)
+{
+	DefaultPawnClass = AAPersonal_Hub_Character::StaticClass();
+	HUDClass = AAPersonal_HUD::StaticClass();
+	PlayerControllerClass = AAPersonal_Player_Controller::StaticClass();
+}
+//-------------------------------------------------------------------------------------------------------------
+void AAPersonal_Hub_Game_Mode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Personal_HUD = Cast<AAPersonal_HUD>(GetWorld()->GetFirstPlayerController()->GetHUD() );
+	//Personal_HUD->HUD_Handler(EWidget_Action::Is_New_Game, );
+	//UGameUserSettings* UserSettings = GEngine->GetGameUserSettings();
+
+	//UserSettings->SetFullscreenMode(EWindowMode::WindowedFullscreen);
+	//UserSettings->SetFrameRateLimit(30.0);
+	//UserSettings->ApplySettings(true);
+}
+//-------------------------------------------------------------------------------------------------------------
+
+*/
--- Source/Module_UI/Public/Module_UI_HUD.h
+++ Source/Module_UI/Public/Module_UI_HUD.h
@@ -15,3 +15,187 @@ public:
 	
 };
 //------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------
+
+
+
+/*
+
+#pragma once
+
+#include "Personal_Hub_Interaction.h"  // Interface
+#include "Blueprint/UserWidget.h"
+#include "Blueprint/DragDropOperation.h"
+#include "Blueprint/WidgetBlueprintLibrary.h"
+
+#include "GameFramework/GameModeBase.h"
+#include "Personal_Hub_Game_Mode.generated.h"
+
+//-----------------------------------------------------------------------------------------------------------
+UENUM(BlueprintType) enum class EHUD_State : uint8
+{
+	None,
+	Is_New_Game,
+	Toogle_Menu_Main,
+	Toogle_Inventory,
+	Toogle_Menu_Option,
+	Update_Morph
+	//	Main_Menu_HUD UMETA(DisplayName = "Main_Menu_HUD"),
+};
+//-----------------------------------------------------------------------------------------------------------
+
+
+
+
+// UADrag_Drop_Operation
+class USizeBox;
+class UImage;
+class UProgressBar;
+class UBorder;
+//-----------------------------------------------------------------------------------------------------------
+UCLASS(meta = (DisableNativeTick)) class UADrag_Drop_Operation : public UDragDropOperation
+{// This specialized Class enables us to pass through information as part of our drag and drop action.
+
+	GENERATED_BODY()
+
+public:
+	FVector2D Drag_Offset;  // offsets the location from where we start dragging and dropping the widget.
+	UUserWidget* Widget_Reference;  // used to hold the UMG Widget that we want to drag around on screen.
+};
+//-----------------------------------------------------------------------------------------------------------
+
+
+
+
+// UADrag_Widget
+UCLASS(meta = (DisableNativeTick)) class UADrag_Widget : public UUserWidget
+{// Draged Visual Widget, Used to show where we can drag our widget
+
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct();
+	void Update_State(const UImage* image, const FVector2D& desired_size) const;
+
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<USizeBox> SizeBox_Root;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UBorder> Border_Root;
+};
+//-----------------------------------------------------------------------------------------------------------
+
+
+
+
+// UAPersonal_Main_Button
+UCLASS(meta = (DisableNativeTick)) class UAPersonal_Main_Button : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct();
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& in_geometry, const FPointerEvent& in_mouse_event);  // Handle LMB click on this widget
+	virtual void NativeOnDragDetected(const FGeometry& in_geometry, const FPointerEvent& in_mouse_event, UDragDropOperation*& out_operation);  // Create operation data to save, and use when drop
+	virtual bool NativeOnDrop(const FGeometry& in_geometry, const FDragDropEvent& in_drag_drop_event, UDragDropOperation* in_operation);  // What to do while on drop widget
+
+	void Init() const;  // Is everything we need to work correct?
+
+	FVector2D Drag_Offset;
+	TObjectPtr<UADrag_Drop_Operation> Drag_Drop_Operation;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Widget | Event") void Update_State();  // Update Image
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget | Param", meta = (ExposeOnSpawn = true)) bool Is_Empty;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget | Components", meta = (BindWidget)) TObjectPtr<USizeBox> SizeBox_Root;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget | Components", meta = (BindWidget)) TObjectPtr<UImage> Image_Root;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget | Class", meta = (ExposeOnSpawn = true)) TSubclassOf<UADrag_Widget> Drag_Widget_Class;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget | Class", meta = (ExposeOnSpawn = true)) TSubclassOf<UADrag_Drop_Operation> Drag_Drop_O_Class;
+};
+//-----------------------------------------------------------------------------------------------------------
+
+
+
+
+// UAPersonal_Main_Menu
+UCLASS(meta = (DisableNativeTick)) class UAPersonal_Main_Menu : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct();
+};
+//-----------------------------------------------------------------------------------------------------------
+
+
+
+
+// UAPersonal_Main_HUD
+UCLASS(meta = (DisableNativeTick)) class UAPersonal_Main_HUD : public UUserWidget
+{// Create Main Menu, Settings Option + Customization(Morph)
+
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct();
+	virtual bool NativeOnDrop(const FGeometry& in_geometry, const FDragDropEvent& in_drag_drop_event, UDragDropOperation* in_operation);  // What to do while widget drop on
+
+	void Handler(const EWidget_Action widget_main_hud, void* ptr = 0);  // Controll all widgets from here
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Event | Handler") void Handler_State_HUD(EHUD_State hud_state_update);  // Call needed func from BP
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Props | State", meta = (AllowPrivateAccess = "true")) EHUD_State EHUD_Status;
+
+private:
+	void Update_Morph_Sliders();  // Update Sliders in Widgets BP
+
+	double* WMorph_Param_Array;
+	std::function<void()>* Update_Morph;
+	UADrag_Drop_Operation* Drag_N_Drop_Operation;
+
+	UFUNCTION(BlueprintCallable, Category = "Widget | Morph") void Handle_Game_State();  // Update from BP Slider to WMorph_Param_Array
+	UFUNCTION(BlueprintCallable, Category = "Widget | Morph") void Set_Morph_State(const int index, double value);  // Update from BP Slider to WMorph_Param_Array
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Props | Morph", meta = (AllowPrivateAccess = "true")) TArray<double> Morphs_Param;
+};
+//-----------------------------------------------------------------------------------------------------------
+
+
+
+
+// AAPersonal_HUD
+UCLASS(meta = (DisableNativeTick)) class AAPersonal_HUD : public AHUD, public IIPersonal_Hub_Interaction
+{
+	GENERATED_BODY()
+
+public:
+	AAPersonal_HUD();
+
+	virtual void BeginPlay();
+	virtual void HUD_Handler(const EWidget_Action& widgete_action, void* ptr);  // Say widget what to do, and set params
+
+private:
+	EWidget_Action* Widget_HUD_State;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Widget") TObjectPtr<UAPersonal_Main_HUD> Widget_HUD;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Widget") TSubclassOf<UUserWidget> Widget_HUD_Class;
+};
+//-----------------------------------------------------------------------------------------------------------
+
+
+
+
+// APersonalHubGameMode
+UCLASS(minimalapi) class AAPersonal_Hub_Game_Mode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAPersonal_Hub_Game_Mode();
+
+	virtual void BeginPlay();
+
+private:
+	AAPersonal_HUD* Personal_HUD;
+};
+//-------------------------------------------------------------------------------------------------------------
+
+*/

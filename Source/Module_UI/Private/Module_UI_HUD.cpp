daba733a0a369e61327fe7286b8d520bf3ef992c
Andrey <getboros@gmail.com>
2025-05-06 16:35:10 +0300

    HUD, refactoring

--- Source/Module_UI/Private/Module_UI_HUD.cpp
+++ Source/Module_UI/Private/Module_UI_HUD.cpp
@@ -75,195 +75,48 @@ bool UAModule_UI_Inventory_Slot::NativeOnDrop(const FGeometry &in_geometry, cons
 
 
 
-// UModule_UI_HUD
-void UModule_UI_HUD::NativeOnDragDetected(const FGeometry &in_geometry, const FPointerEvent &in_mouse_event, UDragDropOperation *&out_operation)
+// AAModule_UI_HUD 
+#include "Engine/AssetManager.h"
+//-----------------------------------------------------------------------------------------------------------
+void AAModule_UI_HUD::BeginPlay()
 {
-	Super::NativeOnDragDetected(in_geometry, in_mouse_event, out_operation);
-}
-//------------------------------------------------------------------------------------------------------------
-FReply UModule_UI_HUD::NativeOnMouseButtonDown(const FGeometry &in_geometry, const FPointerEvent &in_mouse_event)
-{
-	Super::NativeOnMouseButtonDown(in_geometry, in_mouse_event);
+    Super::BeginPlay();
 
-    const FVector2D mouse_pos = in_geometry.AbsoluteToLocal(in_mouse_event.GetScreenSpacePosition() );
+    if (MainMenuClass)
+    {
+        MainMenuWidget = CreateWidget<UUserWidget>(GetWorld(), MainMenuClass);
+        if (MainMenuWidget)
+            MainMenuWidget->AddToViewport(0);
+    }
 
-    UE_LOG(LogTemp, Warning, TEXT("Mouse Clicked at: X: %f, Y: %f"), mouse_pos.X, mouse_pos.Y);
+    if (InventoryWidgetAsset.IsValid() == false)
+        StreamableManager.RequestAsyncLoad(InventoryWidgetAsset.ToSoftObjectPath(), FStreamableDelegate::CreateUObject(this, &AAModule_UI_HUD::OnInventoryLoaded) );
+    else
+        OnInventoryLoaded();
 
-	return UWidgetBlueprintLibrary::DetectDragIfPressed(in_mouse_event, this, EKeys::LeftMouseButton).NativeReply;  // if start drag
-}
-//------------------------------------------------------------------------------------------------------------
-FReply UModule_UI_HUD::NativeOnMouseMove(const FGeometry &in_geometry, const FPointerEvent &in_mouse_event)
-{
-    const FVector2D mouse_pos = in_mouse_event.GetScreenSpacePosition();
-
-    UE_LOG(LogTemp, Warning, TEXT("Mouse Moved at: X: %f, Y: %f"), mouse_pos.X, mouse_pos.Y);
-
-    return FReply::Unhandled();
-}
-//------------------------------------------------------------------------------------------------------------
-
-
-/*
-
-#include "Personal_Hub_Game_Mode.h"
-#include "Personal_Hub_Character.h"
-
-#include "Components/ProgressBar.h"
-#include "GameFramework/GameUserSettings.h"
-
-
-
-
-
-// UAPersonal_Main_Menu
-void UAPersonal_Main_Menu::NativeConstruct()
-{
-	Super::NativeConstruct();
-}
-//-------------------------------------------------------------------------------------------------------------
-
-
-
-
-// UAPersonal_Main_HUD
-void UAPersonal_Main_HUD::NativeConstruct()
-{
-	WMorph_Param_Array = 0;
-	EHUD_Status = EHUD_State::None;
-
-	Super::NativeConstruct();
-}
-//-------------------------------------------------------------------------------------------------------------
-bool UAPersonal_Main_HUD::NativeOnDrop(const FGeometry& in_geometry, const FDragDropEvent& in_drag_drop_event, UDragDropOperation* in_operation)
-{
-	UUserWidget* slot_widget = 0;  // need to save after
-	FVector2D view_pos = {};
-
-	Super::NativeOnDrop(in_geometry, in_drag_drop_event, in_operation);
-
-	Drag_N_Drop_Operation = Cast<UADrag_Drop_Operation>(in_operation);  // or UADrag_Drop_Operation
-	slot_widget = Drag_N_Drop_Operation->Widget_Reference;
-	slot_widget->RemoveFromParent();
-	slot_widget->AddToViewport();
-	view_pos = in_geometry.AbsoluteToLocal(in_drag_drop_event.GetScreenSpacePosition()) - Drag_N_Drop_Operation->Drag_Offset;  // Set where to place location
-	slot_widget->SetPositionInViewport(view_pos, false);
-
-	return true;
-}
-//-------------------------------------------------------------------------------------------------------------
-void UAPersonal_Main_HUD::Handler(const EWidget_Action widget_main_hud, void* ptr)
-{
-	switch (widget_main_hud)
-	{
-	case EWidget_Action::None:
-		break;
-
-
-	case EWidget_Action::Init_Morph_Param:
-		WMorph_Param_Array = static_cast<double*>(ptr);  // If action Morph convert to double array, Save to Morphs_Options and send to widget
-		Update_Morph_Sliders();
-		break;
-
-	case EWidget_Action::Init_Morph_Func:
-		Update_Morph = static_cast<std::function<void()> *>(ptr);
-		break;
-
-	case EWidget_Action::Update_Morph_Slider:
-		Update_Morph_Sliders();
-		break;
-
-	case EWidget_Action::Menu_Main_Toogle:
-		Handler_State_HUD(EHUD_State::Toogle_Menu_Main);
-		break;
-
-	case EWidget_Action::Inventory_Toogle:
-		Handler_State_HUD(EHUD_State::Toogle_Inventory);
-		break;
-	default:
-		break;
-	}
-}
-//-------------------------------------------------------------------------------------------------------------
-void UAPersonal_Main_HUD::Update_Morph_Sliders()
-{
-	for (size_t i = 0; i < 5; i++)
-		Morphs_Param.Add(WMorph_Param_Array[i]);
-
-	Handler_State_HUD(EHUD_State::Update_Morph);  // In BP Call update morps
-}
-//-------------------------------------------------------------------------------------------------------------
-void UAPersonal_Main_HUD::Handle_Game_State()
-{
-	(*Update_Morph)();
-}
-//-------------------------------------------------------------------------------------------------------------
-void UAPersonal_Main_HUD::Set_Morph_State(const int index, double value)
-{
-	WMorph_Param_Array[index] = value;
-	(*Update_Morph)();
-}
-//------------------------------------------------------------------------------------------------------------
-
-
-
-
-// AAPersonal_HUD
-AAPersonal_HUD::AAPersonal_HUD()
-	: Widget_HUD_State(), Widget_HUD(0), Widget_HUD_Class(0)
-{
+    if (StatsWidgetAsset.IsValid() == false)
+        StreamableManager.RequestAsyncLoad(StatsWidgetAsset.ToSoftObjectPath(), FStreamableDelegate::CreateUObject(this, &AAModule_UI_HUD::OnStatsLoaded) );
+    else
+        OnStatsLoaded();
 }
 //-----------------------------------------------------------------------------------------------------------
-void AAPersonal_HUD::BeginPlay()
+void AAModule_UI_HUD::OnInventoryLoaded()
 {
-	//if (!Widget_HUD_Class != 0)
-	//	return;
-	//Widget_HUD = CreateWidget<UAPersonal_Main_HUD>(GetWorld(), Widget_HUD_Class);
-	//Widget_HUD->AddToViewport();
-
-	Super::BeginPlay();
+    UClass* LoadedClass = InventoryWidgetAsset.Get();
+    if (LoadedClass && InventoryWidget == nullptr)
+    {
+        InventoryWidget = CreateWidget<UUserWidget>(GetWorld(), LoadedClass);
+        InventoryWidget->AddToViewport(1);
+    }
 }
 //-----------------------------------------------------------------------------------------------------------
-void AAPersonal_HUD::HUD_Handler(const EWidget_Action& widgete_action, void* ptr)
+void AAModule_UI_HUD::OnStatsLoaded()
 {
-	switch (widgete_action)
-	{
-	case EWidget_Action::None:
-		Widget_HUD_State = static_cast<EWidget_Action*>(ptr);  // Initialize
-		break;
-	case EWidget_Action::Update_HUD_State:
-		*Widget_HUD_State = (EWidget_Action)Widget_HUD->EHUD_Status;
-		break;
-	default:
-		Widget_HUD->Handler(widgete_action, ptr);
-		break;
-	}
+    UClass* LoadedClass = StatsWidgetAsset.Get();
+    if (LoadedClass && StatsWidget == nullptr)
+    {
+        StatsWidget = CreateWidget<UUserWidget>(GetWorld(), LoadedClass);
+        StatsWidget->AddToViewport(2);
+    }
 }
 //-----------------------------------------------------------------------------------------------------------
-
-
-
-
-// APersonalHubGameMode
-AAPersonal_Hub_Game_Mode::AAPersonal_Hub_Game_Mode()
-	: Personal_HUD(0)
-{
-	DefaultPawnClass = AAPersonal_Hub_Character::StaticClass();
-	HUDClass = AAPersonal_HUD::StaticClass();
-	PlayerControllerClass = AAPersonal_Player_Controller::StaticClass();
-}
-//-------------------------------------------------------------------------------------------------------------
-void AAPersonal_Hub_Game_Mode::BeginPlay()
-{
-	Super::BeginPlay();
-
-	//Personal_HUD = Cast<AAPersonal_HUD>(GetWorld()->GetFirstPlayerController()->GetHUD() );
-	//Personal_HUD->HUD_Handler(EWidget_Action::Is_New_Game, );
-	//UGameUserSettings* UserSettings = GEngine->GetGameUserSettings();
-
-	//UserSettings->SetFullscreenMode(EWindowMode::WindowedFullscreen);
-	//UserSettings->SetFrameRateLimit(30.0);
-	//UserSettings->ApplySettings(true);
-}
-//-------------------------------------------------------------------------------------------------------------
-
-*/
--- Source/Module_UI/Public/Module_UI_HUD.h
+++ Source/Module_UI/Public/Module_UI_HUD.h
@@ -2,6 +2,8 @@
 
 #include "Blueprint/DragDropOperation.h"
 #include "Blueprint/UserWidget.h"
+#include "GameFramework/HUD.h"
+#include "Engine/StreamableManager.h"
 
 #include "Module_UI_HUD.generated.h"
 
@@ -45,6 +47,7 @@ public:
 	virtual bool NativeOnDrop(const FGeometry &in_geometry, const FDragDropEvent &in_drag_drop_event, UDragDropOperation *in_operation);  // What to do while on drop widget
 	//virtual FReply NativeOnMouseMove(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent);
 
+	int Item_Index;  // Store ID Inventory_Item in Player
 	FVector2D Drag_Offset;
 	TObjectPtr<UADrag_Drop_Operation> Drag_Drop_Operation;
 
@@ -56,109 +59,24 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget | Class", meta = (ExposeOnSpawn = true) ) TSubclassOf<UADrag_Drop_Operation> Drag_Drop_O_Class;
 };
 //-----------------------------------------------------------------------------------------------------------
-UCLASS(meta = (DisableNativeTick) ) class MODULE_UI_API UModule_UI_HUD : public UUserWidget
+UCLASS(meta = (DisableNativeTick) ) class MODULE_UI_API AAModule_UI_HUD : public AHUD
 {
 	GENERATED_BODY()
 
 public:
-	virtual void NativeOnDragDetected(const FGeometry &in_geometry, const FPointerEvent &in_mouse_event, UDragDropOperation *&out_operation);
-	virtual FReply NativeOnMouseButtonDown(const FGeometry &in_geometry, const FPointerEvent &in_mouse_event);
-	virtual FReply NativeOnMouseMove(const FGeometry &in_geometry, const FPointerEvent &in_mouse_event);
+	virtual void BeginPlay();
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget | Components", meta = (BindWidget) ) TObjectPtr<UImage> Image_Root;
+	UPROPERTY(EditDefaultsOnly, Category = "UI") TSubclassOf<UUserWidget> MainMenuClass;
+	UPROPERTY(EditDefaultsOnly, Category = "UI") TSoftClassPtr<UUserWidget> InventoryWidgetAsset;
+	UPROPERTY(EditDefaultsOnly, Category = "UI") TSoftClassPtr<UUserWidget> StatsWidgetAsset;
+
+	UPROPERTY() UUserWidget* MainMenuWidget;
+	UPROPERTY() UUserWidget* InventoryWidget;
+	UPROPERTY() UUserWidget* StatsWidget;
+	
+	FStreamableManager StreamableManager;
+
+	void OnInventoryLoaded();
+	void OnStatsLoaded();
 };
 //------------------------------------------------------------------------------------------------------------
-
-
-
-/*
-
-#pragma once
-
-#include "Personal_Hub_Interaction.h"  // Interface
-#include "Blueprint/UserWidget.h"
-#include "Blueprint/WidgetBlueprintLibrary.h"
-
-#include "GameFramework/GameModeBase.h"
-#include "Personal_Hub_Game_Mode.generated.h"
-
-//-----------------------------------------------------------------------------------------------------------
-UENUM(BlueprintType) enum class EHUD_State : uint8
-{
-	None,
-	Is_New_Game,
-	Toogle_Menu_Main,
-	Toogle_Inventory,
-	Toogle_Menu_Option,
-	Update_Morph
-	//	Main_Menu_HUD UMETA(DisplayName = "Main_Menu_HUD"),
-};
-//-----------------------------------------------------------------------------------------------------------
-UCLASS(meta = (DisableNativeTick)) class UAPersonal_Main_Menu : public UUserWidget
-{
-	GENERATED_BODY()
-
-public:
-	virtual void NativeConstruct();
-};
-//-----------------------------------------------------------------------------------------------------------
-UCLASS(meta = (DisableNativeTick)) class UAPersonal_Main_HUD : public UUserWidget
-{// Create Main Menu, Settings Option + Customization(Morph)
-
-	GENERATED_BODY()
-
-public:
-	virtual void NativeConstruct();
-	virtual bool NativeOnDrop(const FGeometry& in_geometry, const FDragDropEvent& in_drag_drop_event, UDragDropOperation* in_operation);  // What to do while widget drop on
-
-	void Handler(const EWidget_Action widget_main_hud, void* ptr = 0);  // Controll all widgets from here
-
-	UFUNCTION(BlueprintImplementableEvent, Category = "Event | Handler") void Handler_State_HUD(EHUD_State hud_state_update);  // Call needed func from BP
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Props | State", meta = (AllowPrivateAccess = "true")) EHUD_State EHUD_Status;
-
-private:
-	void Update_Morph_Sliders();  // Update Sliders in Widgets BP
-
-	double* WMorph_Param_Array;
-	std::function<void()>* Update_Morph;
-	UADrag_Drop_Operation* Drag_N_Drop_Operation;
-
-	UFUNCTION(BlueprintCallable, Category = "Widget | Morph") void Handle_Game_State();  // Update from BP Slider to WMorph_Param_Array
-	UFUNCTION(BlueprintCallable, Category = "Widget | Morph") void Set_Morph_State(const int index, double value);  // Update from BP Slider to WMorph_Param_Array
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Props | Morph", meta = (AllowPrivateAccess = "true")) TArray<double> Morphs_Param;
-};
-//-----------------------------------------------------------------------------------------------------------
-UCLASS(meta = (DisableNativeTick)) class AAPersonal_HUD : public AHUD, public IIPersonal_Hub_Interaction
-{
-	GENERATED_BODY()
-
-public:
-	AAPersonal_HUD();
-
-	virtual void BeginPlay();
-	virtual void HUD_Handler(const EWidget_Action& widgete_action, void* ptr);  // Say widget what to do, and set params
-
-private:
-	EWidget_Action* Widget_HUD_State;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Widget") TObjectPtr<UAPersonal_Main_HUD> Widget_HUD;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Widget") TSubclassOf<UUserWidget> Widget_HUD_Class;
-};
-//-----------------------------------------------------------------------------------------------------------
-UCLASS(minimalapi) class AAPersonal_Hub_Game_Mode : public AGameModeBase
-{
-	GENERATED_BODY()
-
-public:
-	AAPersonal_Hub_Game_Mode();
-
-	virtual void BeginPlay();
-
-private:
-	AAPersonal_HUD* Personal_HUD;
-};
-//-------------------------------------------------------------------------------------------------------------
-
-*/
